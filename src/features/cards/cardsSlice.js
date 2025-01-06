89ee7c93ccfbac0bbc315420842368597547f198
27Sasha <134843865+27Sasha@users.noreply.github.com>
2025-01-06 14:13:14 +1100

    Update cardsSlice.js

--- src/features/cards/cardsSlice.js
+++ src/features/cards/cardsSlice.js
@@ -1,20 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
 
-export const cardsSlice = createSlice({
-    name: 'cards',
-    initialState: {cards: {}},
-    reducers: {
-        addCard: (state, action) => {
-            const { cardId, front, back } = action.payload;
-            state.cards[cardId] = {
-                id: cardId,
-                front: front,
-                back: back
-            }
-        }
+const cardsSlice = createSlice({
+  name: 'cards',
+  intialState:{
+    cards: {}
+  },
+  reducer:{
+    addCard: (state,action) =>{
+      const {id, front, back} = action.payload
+      state.cards[id] = ({id: id, front: front, back: back})
     }
-});
+  }
+})
 
-export const { addCard } = cardsSlice.actions;
-export default cardsSlice.reducer;
-export const selectCard = state => state.cards.cards;
+export const selectCard = (id) = (state) => state.cards.cards
+export const { addCards } = cardsSlice.action
+export default cardsSlice.reducer
