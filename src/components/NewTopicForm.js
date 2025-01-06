8504aaf6f12f1c65f2f74f762c56ea0919220d79
27Sasha <134843865+27Sasha@users.noreply.github.com>
2025-01-06 14:12:25 +1100

    Update NewTopicForm.js

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -1,31 +1,28 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
+import { useDispatch } from "react-redux";
+import {useNavigate } from "react-router-dom";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
-import { addTopic } from '../features/topics/topicsSlice';
-import { useDispatch } from "react-redux";
+// import addTopic
+import { addTopic } from '../features/topics/topicsSlice'
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default function NewTopicForm() {
+  const dispatch = useDispatch();
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
-  const history = useHistory();
-  const dispatch = useDispatch();
-
+  const navigate = useNavigate()
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name.length === 0) {
       return;
     }
-    let uniqueId = uuidv4();
-    dispatch(addTopic({
-        topicId: uniqueId,
-        name: name,
-        icon: icon
-      }));
 
-    // dispatch your add topic action here
-    history.push(ROUTES.topicsRoute());
+    // dispatch new topic
+    dispatch(addTopic({id: uuidv4(), name:name, icon: icon}))
+    navigate(ROUTES.topicsRoute());
+    
   };
 
   return (
@@ -55,8 +52,9 @@ export default function NewTopicForm() {
             ))}
           </select>
         </div>
-        <button className="center">Add Topic</button>
+        <button className="center" type="submit">Add Topic</button>
       </form>
     </section>
   );
 }
+
