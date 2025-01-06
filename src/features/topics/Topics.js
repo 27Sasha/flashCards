72efd5bec69fae38051b618a2b51aee45164ef9a
27Sasha <134843865+27Sasha@users.noreply.github.com>
2025-01-06 14:14:50 +1100

    Update Topics.js

--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -1,9 +1,9 @@
-import NewTopicForm from "../../components/NewTopicForm";
+import React from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
-import { useSelector } from 'react-redux';
-import { selectTopics } from './topicsSlice';
-
+// import selector
+import {selectTopics} from './topicsSlice'
+import { useSelector } from "react-redux";
 
 export default function Topics() {
   const topics = useSelector(selectTopics); // replace this with a call to your selector to select all the topics in state
