2862224084c172464a506f3db3a6992057ebd4d9
27Sasha <134843865+27Sasha@users.noreply.github.com>
2025-01-07 15:36:14 +1100

    Update Topic.js

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -1,23 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useParams, Navigate } from "react-router-dom";
+import ROUTES from "../../app/routes";
+// import selectors
+import {selectQuizz} from ''../features/quizzes/quizzesSlice''
+import {selectTopics} from ''../features/topics/topicsSlice''
 
-const quizzesSlice= createSlice({
-  name:'quizzes',
-  initialState: {
-    quizzes:{}
-  },
-  reducers:{
-    addQuiz: (state,action) =>{
-      const {id, name, topicId, cardIds} = action.payload
-      state.quizzes[id] = {
-        id,
-        name,
-        topicId,
-        cardIds
-      }
-    }
+export default function Topic() {
+  const topics = useSelector(selectTopics);  // replace with selector
+  const quizzes = useSelector(selectQuizz); // replace with selector
+  const { topicId } = useParams();
+  const topic = topics[topicId];
+
+  if(!topic) {
+    return <Navigate to={ROUTES.topicsRoute()} replace/>
   }
-})
+  
+  const quizzesForTopic = topic.quizIds.map((quizId) => quizzes[quizId]);
 
-export const selectQuizz = (state) = state.quizzes.quizzes
-export const {addQuiz } = quizzesSlice.actions
-export default quizzesSlice.reducers
+  return (
+    <section>
+      <img src={topic.icon} alt="" className="topic-icon" />
+      <h1>{topic.name}</h1>
+      <ul className="quizzes-list">
+        {quizzesForTopic.map((quiz) => (
+          <li className="quiz" key={quiz.id}>
+            <Link to={ROUTES.quizRoute(quiz.id)}>{quiz.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/quizzes/new" className="button center">
+        Create a New Quiz
+      </Link>
+    </section>
+  );
+}
